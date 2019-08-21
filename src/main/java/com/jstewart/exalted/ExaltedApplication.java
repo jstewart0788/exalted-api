1bc142e426e536704f0c7ad1a86b4789e8c27fca
Jonathan Stewart <jstewart0788@gmail.com>
2019-08-21 19:02:18 -0400

    new base with postgres libraries

---
+++ src/main/java/com/jstewart/exalted/ExaltedApplication.java
@@ -0,0 +1,13 @@
+package com.jstewart.exalted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaltedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaltedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jstewart/exalted/ExaltedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstewart.exalted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaltedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
